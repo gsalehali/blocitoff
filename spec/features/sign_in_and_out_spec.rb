c82b1b1b648af887066e7b70546cffa77cb39e34
gsalehali <me@salehali.net>
2016-02-26 15:21:43 +0300

    finished sign in and out

---
+++ spec/features/sign_in_and_out_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+describe "Sign in flow" do
+  before do
+    @user = create(:user)
+  end
+
+  context "valid input" do
+    it "sign in successfully" do
+      visit_root_and_sign_in
+      expect(page.has_content?("Signed in successfully")).to be_truthy
+    end
+  end
+
+  context "invalid input" do
+    it "should display error message" do
+      visit_root_and_sign_in("error@example.com")
+      expect(page.has_content?("Invalid email or password")).to be_truthy
+    end
+  end
+
+  describe "Sign out flow" do
+    it "sign user out successfully" do
+      @user = create(:user)
+      visit_root_and_sign_in
+      page.click_link('', :href => '/users/sign_out')
+      expect(page.has_content?("Signed out successfully")).to be_truthy
+    end
+  end
+end
+
+def visit_root_and_sign_in(email=@user.email, password=@user.password)
+  visit root_path
+
+  within '.user-info' do
+    click_link 'Sign in'
+  end
+
+  fill_in 'Email',      with: email
+  fill_in 'Password',   with: password
+
+  within 'form' do
+    click_button "Sign in"
+  end
+end
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -4,11 +4,7 @@ describe "Sign up flow"  do
     
   context "Vallid inputs" do 
     it "allow new user to sign up with valid infromation"  do
-      visit "users/sign_up"
-
-      fill_in_information
-
-      click_button "Sign up" 
+      visit_rgistration_and_fill_in_information
 
       expect(page.has_css?(".alert-success", text: "A message with a confirmation link has been sent to your email address")).to be_truthy
     end
@@ -16,31 +12,27 @@ describe "Sign up flow"  do
 
   context "Invalid inputs" do
     it "doesn't allow invalid email addresses" do
-      visit "users/sign_up"
-
-      fill_in_information("")
-
-      click_button "Sign up"
+      visit_rgistration_and_fill_in_information("")
 
       expect(page.has_content?("Email can't be blank")).to be_truthy
     end
 
     it " doesn't allow invalid passwords" do
-      visit "users/sign_up"
+      visit_rgistration_and_fill_in_information(nil, "12345678")
 
-      fill_in_information(nil, "12345678")
-
-      click_button "Sign up"
-
-      expect(page.has_content?("Email can't be blank")).to be_truthy
+      expect(page.has_content?("Password confirmation doesn't match Password")).to be_truthy
 
     end
   end
 end
 
-def fill_in_information(email="test@example.com", password ="helloworld", password_confirmation="helloworld")
+def visit_rgistration_and_fill_in_information(email="test@example.com", password="helloworld", password_confirmation="helloworld")
+  visit "users/sign_up"
+
   fill_in "Email",                  with: email
   fill_in "Password",               with: password
   fill_in "Password confirmation",  with: password_confirmation
+
+  click_button "Sign up"
 end
   
