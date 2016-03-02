a7b6ded024a9991becce2d38362ac29b72ef6954
gsalehali <me@salehali.net>
2016-03-02 19:00:45 +0300

    Added creating and removeing with Ajax

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,19 +1,43 @@
 class ItemsController < ApplicationController
 
   def create
+    @user = User.find(params[:user_id])
     @item = Item.new(item_params)
     @item.user = current_user
+    @new_item = Item.new 
 
     if @item.save
       flash[:notice] = "A new item has been added to your list."
-      redirect_to user_path(current_user)
+      # redirect_to user_path(current_user)
     else
       errors = ""
       @item.errors.full_messages.each do |msg|
         errors = "<li>#{msg}</li>" + errors
       end
       flash[:error] = "There was an error adding your item. Please try again! <ul>#{errors}<ul>"
-      redirect_to user_path(current_user)
+      # redirect_to user_path(current_user)
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+
+    if @item.destroy
+      flash[:notice] = "Item has been marked as completed and deleted"
+      # redirect_to user_path(@item.user)
+    else
+      flash[:error] = "There was an error. Please try again."
+      # reidrect_to user_path(@item.user)
+    end
+    
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:update, :show] do
-    resources :items, only: [:create]
+    resources :items, only: [:create, :destroy]
   end
   root 'welcome#index'
 end
