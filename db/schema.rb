581f7eb5dd63d0ae096853aacfdb1644802e8ebe
Xander Miller <xander.miller@gmail.com>
2016-03-02 14:03:19 -0500

    adds shared examples for testing attributes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160226162141) do
+ActiveRecord::Schema.define(version: 20160219135508) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 20160226162141) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,27 @@ describe User do
   end
 
   ## Attributes
+  it_behaves_like "it has attributes", User,
+  {
+    id: :integer,
+    name: :string,
+    email: :string,
+    encrypted_password: :string,
+    reset_password_token: :string,
+    reset_password_sent_at: :datetime,
+    remember_created_at: :datetime,
+    sign_in_count: :integer,
+    current_sign_in_at: :datetime,
+    last_sign_in_at: :datetime,
+    current_sign_in_ip: :string,
+    last_sign_in_ip: :string,
+    confirmation_token: :string,
+    confirmed_at: :datetime,
+    confirmation_sent_at: :datetime,
+    unconfirmed_email: :string,
+    created_at: :datetime,
+    updated_at: :datetime
+  } {}
 
   ## Validations
 
