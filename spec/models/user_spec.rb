7c9edd4b82d371304282e8d3c307a33c64ebaa94
Xander Miller <xander.miller@gmail.com>
2016-03-02 13:16:13 -0500

    adds shared specs helper and documentation format

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
-require 'rails_helper'
+require 'support/model_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  it "has a valid factory" do
+    expect(build(:user)).to be_valid
+  end
+
+  ## Attributes
+
+  ## Validations
+
+  ## Class Methods
+
+  ## Instance Methods
+
 end
