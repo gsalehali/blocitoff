c98909634dda5f65012867115f47ef0d7efbe79d
gsalehali <me@salehali.net>
2016-03-03 22:04:41 +0300

    display time remaining

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ActiveRecord::Base
   belongs_to :user
   validates :name, length: {minimum: 5}, presence: true
   validates :user, presence: true
+
+  def days_left
+    7 - (DateTime.now.to_date - created_at.to_date).to_i
+  end
 end
